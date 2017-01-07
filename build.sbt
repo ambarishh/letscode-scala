50afd3e05c98c08a020fb9d266990172a7d78036
ambarish <ahazarnis@paypal.com>
2017-01-06 16:16:19 -0800

    Initial Commit

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "letscode-scala"
+
+version := "1.0"
+
+scalaVersion := "2.10.2"
+    
---
+++ src/main/scala/MyApp.scala
@@ -0,0 +1,6 @@
+/**
+  * Created by ahazarnis on 1/6/17.
+  */
+object MyApp extends App{
+  println("Hello Coders")
+}
